31643ed0e0e965757f3e59e38575737d91747d06
Esper06 <matt.walford98@gmail.com>
2021-09-14 21:35:48 +0100

    Added server connection to server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = required("path"); //we bring in path
+const express = required("express"); //we bring in express
+const dbConnection = require("./config/connection"); //we bring in code from connection.js
+
+const app = express(); //we create a variable to run express
+const PORT = process.env.PORT || 3000; //we then set a port to be used. It's 3000 by default
+
+app.use(express.json()); //Makes it so that we can take in json responses
+app.use(express.urlencoded({ extended: true })); //Makes it so that we can take in special url characters
+app.use(express.static(path.join(__dirname, "public"))); //Joins our current directory to public
+
+
+dbConnection.sync({ force: false }).then(() => { //connects to the database and logs out what port we are connected to
+    app.listen(PORT, () => {
+        console.log("Server listening on port " + PORT);
+    });
+});
+

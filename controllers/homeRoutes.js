68c619171ef588fa22f1373ce7a16dee01996fac
Esper06 <matt.walford98@gmail.com>
2021-09-23 19:18:24 +0100

    Added username handlebars, and a route to it in homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,6 +11,14 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.get("/username", (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+    return;
+  }
+  res.render("username");
+})
+
 router.get("/register", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/");

e7110eb2e7be4eba1834c785ea829d6d3c6300a4
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-09-23 15:56:40 +0100

    add search page and add the selected movie

--- public/js/movies.js
+++ public/js/movies.js
@@ -4,12 +4,7 @@ const searchBtn = $("#searchBtn");
 const youtubeApiKey = "AIzaSyB9ILII2-SnkQFm4eEVSNcNMXvhmg_FcEs";
 const omdbApiKey = "bcb8a4fa";
 
-const viewMore = async (movieName) => {
-  const searchTypeTitle = `t=${movieName}`;
-  const urlTitle = `http://www.omdbapi.com/?${searchTypeTitle}&apikey=${omdbApiKey}&Type=movie`;
-  const movieDetail = await findMovie(urlTitle);
-  return movieDetail;
-};
+
 const findMovie = async (url) => {
   movieList = [];
   try {
@@ -18,10 +13,13 @@ const findMovie = async (url) => {
 
     return data;
   } catch (err) {
-    console.log("qweqwe", err);
+    console.log("Error", err);
   }
 };
 
+const pick_color = () => {
+  return (Math.floor(Math.random() * 6) + 1) * 100;
+};
 searchBtn.on("submit", async (ev) => {
   $("#alertMessage").empty();
   ev.preventDefault();
@@ -30,61 +28,119 @@ searchBtn.on("submit", async (ev) => {
   const url = `http://www.omdbapi.com/?${searchType}&plot=full&apikey=${omdbApiKey}&Type=movie`;
 
   const movieList = await findMovie(url);
+
   if (movieList.Response == "False") {
     errorHandler(movieList.Error);
     return;
   }
-  const movieDetail = await viewMore(movieName);
-  const searchTypeTitle = `t=${movieName}`;
-  const urlTitle = `http://www.omdbapi.com/?${searchTypeTitle}&apikey=${omdbApiKey}&Type=movie`;
-  const movieListTitle = await findMovie(urlTitle);
-  const { Actors, Genre, Plot, Poster } = movieListTitle;
-  log(Poster);
-  const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${movieList.Search[0].Title}  trailer&type=video&key=${youtubeApiKey}`;
-  const vieoLink = await findMovie(youtubeUrl);
-  log(movieList);
 
-  $("#myMovieList").append(
-    `<li class="list-group-item active">${movieList.Search[0].Title}</li>`
-  );
-  for (var i = 1; i < movieList.Search.length; i++) {
-    $("#myMovieList").append(
-      `<li class="list-group-item">${movieList.Search[i].Title}</li>`
+  log("the movie list are ", movieList);
+  $("#SeachContainer")
+    .append(`<div class=" bg-transparent my-2 py-1" role="alert">
+  <h5 class="text-green-200"">Search Result </h2>
+  </div>`);
+  var colorBg = [];
+
+  for (var i = 1; i < movieList.Search.length - 1; i++) {
+    colorList = ["bg-green", "bg-blue", "bg-yellow", "bg-pink", "bg-red"];
+    let btnColor = `${
+      colorList[Math.floor(Math.random() * 4)]
+    }-${pick_color()}`;
+    if (colorBg.includes(btnColor)) {
+      btnColor = `${colorList[Math.floor(Math.random() * 4)]}-${pick_color()}`;
+    } else {
+      colorBg.push(btnColor);
+    }
+
+    $("#movieList").append(
+      `<button type="button" id="movieListItem${i}" class=" btn ${btnColor} m-2">${movieList.Search[i].Title}-${movieList.Search[i].Year}</button>`
     );
+    $(`#movieListItem${i}`).on("click", pickMovie);
   }
 
-  log(
-    `https://www.youtube.com/results?search_query=${vieoLink.items[0].id.videoId}`
+  $(".movieListItem0").click(pickMovie);
+  //  cardCreat(movieListTitle);
+});
+
+const pickMovie = async (event) => {
+  log(event);
+  const myTarget = event.target;
+  log(myTarget.innerHTML);
+  let movieTitle = myTarget.innerHTML.split("-");
+  log(movieTitle);
+
+  let searchTypeTitle = `t=${movieTitle[0]}`;
+
+  const urlTitle = `http://www.omdbapi.com/?${searchTypeTitle}&Year=${movieTitle[1]}&apikey=${omdbApiKey}&Type=movie`;
+  const pickedMovie = await findMovie(urlTitle);
+  log(pickedMovie);
+  if (pickedMovie.Response == "False") {
+    errorHandler(pickedMovie.Error);
+    return;
+  }
+  const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${movieTitle[0]} ${movieTitle[1]} trailer&type=video&key=${youtubeApiKey}`;
+  vieoLink = await findMovie(youtubeUrl);
+  log(vieoLink.items[0].id.videoId);
+  //   $(".trailer").append(
+  //     `
+  //     <div class="embed-responsive embed-responsive-16by9">
+  //   <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${vieoLink.items[0].id.videoId}" allowfullscreen style="width:600px ; height:400px"></iframe>
+  // </div>`);
+  pickedMovie.trailer = `https://www.youtube.com/embed/${vieoLink.items[0].id.videoId}`;
+  cardCreat(pickedMovie);
+};
+const cardCreat = (content) => {
+  $("#result-card").empty();
+  if (content.Poster == "N/A") content.Poster = "./images/noPoster.jpg";
+  $("#result-card").append(
+    `
+    <div class="row d-flex ">
+
+  <div class="col-sm-6">
+      <img class="img-thumbnail rounded mx-auto d-block" src="${content.Poster}" alt="Card image cap">
+  </div>
+
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title text-red-900"><i class="fas fa-step-forward text-green-500"></i> ${content.Title}</i> <span class="text-green-900"> ${content.Year}</span> </h5>
+        <p class="card-text"></i> <i class="fas fa-users"> Actors: </i> ${content.Actors}</p>
+        <p class="card-text"><i class="fas fa-photo-video"> Plot: </i>  ${content.Plot}</p>
+        <p class="card-text bg-red-900 m-0 p-2 text-red-300"></i><i class="fas fa-star "> Rating: </i>  ${content.Ratings[0].Value}</p>
+        
+        <div class="img-thumbnail mx-auto  text-center">
+      <iframe class="embed-responsive-item w-100" src="${content.trailer}" allowfullscreen ></iframe>
+    </div>
+        </div>
+     <button type="submit" class="btn bg-green-400 w-100"> <i class="fas fa-photo-vide"></i> Add to List</button>
+       
+    </div>
+
+  </div>
+
+</div>
+`
   );
 
-  $(".trailer").append(
-    `
-    <div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${vieoLink.items[0].id.videoId}" allowfullscreen ></iframe>
-</div>`
-  );
-  $(".result").append(
-    `<div class="card " >
-  <img class="card-img-top" src="${movieListTitle.Poster}" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">${movieListTitle.Title}<small class="text-muted text-justify-right"> Release Date: ${movieListTitle.Released}</small></h5>
-    <h6 class="card-text">Stars: <small class="text-muted text-justify-right">${movieListTitle.Actors}</small></h6>
-    <h6 class="card-text">Genre: <small class="text-muted text-justify-right">${movieListTitle.Genre}</small></h6>
-    <i class="fa fa-star" aria-hidden="true"></i><span class="card-text">${movieListTitle.Ratings[1].Value}</span>
-    <p class="card-text">${movieListTitle.Plot}</p>
-    <button type="button" class="btn btn-light position-relative mx-3">
-    <i class="fas fa-thumbs-up fs-3 text-success"></i>
-    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-      99+
-    </span>
-</button>
-<button type="button" class="btn btn-light position-relative mx-3">
-    <i class="fas fa-thumbs-down fs-3 text-danger"></i>
-    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-      22
-    </span>
-</button>
-  </div>
-</div>`
-  );
-});
+  //   <div class="row">
+  //   <div class="col-sm-6">
+  //     <div class="card">
+  //       <div class="card-body">
+  //         <h5 class="card-title">Special title treatment</h5>
+  //         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  //         <a href="#" class="btn btn-primary">Go somewhere</a>
+  //       </div>
+  //     </div>
+  //   </div>
+
+  //   <div class="col-sm-6">
+  //     <div class="card">
+  //       <div class="card-body">
+  //         <h5 class="card-title">Special title treatment</h5>
+  //         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  //         <a href="#" class="btn btn-primary">Go somewhere</a>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
+};

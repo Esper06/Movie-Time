59ed16a6748dd97c0454a9077725bc5cf3b30c94
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-09-25 19:46:57 +0100

    remove unnecessary file

--- models/User.js
+++ models/User.js
@@ -1,9 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
-// const youtubeApi = process.env.youtubeApi;
-// const ombdApi = process.env.ombdApi;
-
 
 class User extends Model {
   checkPassword(pwToCheck) {
@@ -38,31 +35,15 @@ User.init(
         len: [8],
       },
     },
-
-    // ombdApi: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    //   defaultValue: ombdApi,
-    // },
-    // youtubeApi: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    //   defaultValue: youtubeApi,
-    //   // AIzaSyAvOiFSZjxuzYcAk91Mw1Whc2c2C7UFrA8
-    // },
   },
   {
     hooks: {
       beforeCreate: async (newUser) => {
         newUser.password = await bcrypt.hash(newUser.password, 10);
-        // newUser.ombdApi = await bcrypt.hash(newUser.ombdApi, 10);
-        // newUser.youtubeApi = await bcrypt.hash(newUser.youtubeApi, 10);
         return newUser;
       },
       beforeUpdate: async (updatedUser) => {
         updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
-        // updatedUser.ombdApi = await bcrypt.hash(updatedUser.ombdApi, 10);
-        // updatedUser.youtubeApi = await bcrypt.hash(updatedUser.youtubeApi, 10);
         return updatedUser;
       },
     },

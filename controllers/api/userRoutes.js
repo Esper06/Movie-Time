1680819446b97c0b4430c9ad8be31ea263f7f5a8
Mehdi <84668631+MehdiMahmud79@users.noreply.github.com>
2021-09-28 18:44:38 +0100

    Update userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -86,6 +86,8 @@ router.post("/login", async (req, res) => {
 });
 
 router.post("/logout", (req, res) => {
+      req.session.logged_in = false;
+
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
